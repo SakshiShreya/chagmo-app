3e94fdf8f69894b045ed9b2e012457cf894c6fea
SakshiShreya <shreyasakshi96@gmail.com>
2019-02-02 00:55:23 +0530

    Added interaction between post and dashboard

--- client/src/app/dashboard/post/post.component.ts
+++ client/src/app/dashboard/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-post',
@@ -7,25 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  // we will actually get this data from server
-  user = {
-    name: "John Smith",
-    img: "assets/images/Profile-Placeholder.png"
-  };
-
-  post = {
-    postType: "Post",
-    data: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    img: "assets/images/placeholder.png",
-    rating: 327,
-    comments: 24,
-    shares: 17
-  }
+  @Input() post;
 
   constructor() { }
 
